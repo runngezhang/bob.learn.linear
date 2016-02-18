096dfa07358d5d9b8d3c1a8e7e163c0a27a05abf
Manuel Gunther <siebenkopf@googlemail.com>
2016-02-18 11:51:39 -0700

    Updated module generation

--- bob/learn/linear/main.cpp
+++ bob/learn/linear/main.cpp
@@ -46,16 +46,15 @@ extern bool init_BobLearnLinearBIC(PyObject* module);
 
 static PyObject* create_module (void) {
 
-  PyBobLearnLinearWhiteningTrainer_Type.tp_new = PyType_GenericNew;
-  if (PyType_Ready(&PyBobLearnLinearWhiteningTrainer_Type) < 0) return 0;
-
 # if PY_VERSION_HEX >= 0x03000000
   PyObject* m = PyModule_Create(&module_definition);
+  auto m_ = make_xsafe(m);
+  const char* ret = "O";
 # else
   PyObject* m = Py_InitModule3(BOB_EXT_MODULE_NAME, module_methods, module_docstr);
+  const char* ret = "N";
 # endif
   if (!m) return 0;
-  auto m_ = make_safe(m);
 
   /* register the types to python */
   if (!init_BobLearnLinearMachine(m)) return 0;
@@ -146,7 +145,7 @@ static PyObject* create_module (void) {
   if (import_bob_io_base() < 0) return 0;
   if (import_bob_learn_activation() < 0) return 0;
 
-  return Py_BuildValue("O", m);
+  return Py_BuildValue(ret, m);
 }
 
 PyMODINIT_FUNC BOB_EXT_ENTRY_NAME (void) {
